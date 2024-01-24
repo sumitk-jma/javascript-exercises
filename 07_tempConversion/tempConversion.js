4fecf8c84822e5a7bdebb14c72972c6daf09f3c0
Sumit Kumar <111978673+sumitk-jma@users.noreply.github.com>
2024-01-24 20:08:00 +0530

    pass the test 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+
+  let temprCel = ((tempF-32)*(5/9));
+  roundedCel = Math.round(temprCel*10)/10
+
+  return roundedCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+  let temprFar = (tempC*(9/5)+32);
+  roundedFar = Math.round(temprFar*10)/10
+
+  return roundedFar;
 };
 
 // Do not edit below this line
