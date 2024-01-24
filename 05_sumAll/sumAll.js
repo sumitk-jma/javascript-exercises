b4e3d828a1a95231f3a3614fc4c6081b27872a03
Sumit Kumar <111978673+sumitk-jma@users.noreply.github.com>
2024-01-24 19:34:10 +0530

    pass the test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(initial, final) {
 
-};
+    if (initial < 0 || final < 0){
+
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(initial) || !Number.isInteger(final)) {
+        
+        return "ERROR";
+
+    }
+    
+    if (initial > final){
+
+        let temp = initial;
+        initial = final;
+        final = temp;
+    }
+    
+    let sum = 0;
+    for (let i = initial; i <= final; i++) {
+      sum += i;
+    }
+    return sum;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
